7663e8696658df49ea7c9cdb5a7be3b640834fea
Sammy Straw <SStraw@carbonite.com>
2018-04-18 17:34:12 -0600

    update

--- liri.js
+++ liri.js
@@ -2,71 +2,43 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
+var mode = process.argv[2];
+var query = process.argv[3];
+
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
-var songSearch = process.argv[2];
+if (mode === "spotify-this-song") {
+  spotify.search({ type: "track", query: query, limit: 3 }, function(err, data) {
 
-spotify.search({ type: "track", query: songSearch, limit: 3 }, function(err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
 
-  if (err) {
-    return console.log("Error occurred: " + err);
-  }
+    console.log("\n~*~*~*~*~*~*~*~*~*~*~\n")
+    console.log("ARTIST NAME: " + JSON.stringify(data.tracks.items[0].album.artists[0].name));
+    console.log("SONG TITLE: " + JSON.stringify(data.tracks.items[0].name));
+    if (data.tracks.items[0].preview_url == null) {
+      console.log("PREVIEW LINK IS UNAVAILABLE");
+    } else {
+      console.log("PREVIEW LINK: " + JSON.stringify(data.tracks.items[0].preview_url));
+    }
+    console.log("ALBUM TITLE: " + JSON.stringify(data.tracks.items[0].album.name))
+    console.log("\n~*~*~*~*~*~*~*~*~*~*~\n")
+  });
 
-  //console.log(JSON.stringify(data, null, 2));
-  console.log("\n~*~*~*~*~*~*~*~*~*~*~\n")
-  console.log("ARTIST NAME: " + JSON.stringify(data.tracks.items[0].album.artists[0].name));
-  console.log("SONG TITLE: " + JSON.stringify(data.tracks.items[0].name));
-  if (JSON.stringify(data.tracks.items[0].preview_url) == null) {
-    console.log("PREVIEW LINK IS UNAVAILABLE" + data.tracks.items[0].preview_url);
-  } else {
-    console.log("PREVIEW LINK: " + JSON.stringify(data.tracks.items[0].preview_url));
-  }
-  console.log("ALBUM TITLE: " + JSON.stringify(data.tracks.items[0].album.name))
-  console.log("\n~*~*~*~*~*~*~*~*~*~*~\n")
-});
-//   var tracksResponseArray = data.tracks.items;
-// 	// Get number of tracks in the response
-// 	var numTracks = tracksResponseArray.length;
+} else if (mode === "my-tweets") {
 
-// 	// Dp the following If the number of tracks is 1 or more.
-// 	if (numTracks > 0) {
-// 		//Map over each track, store necessary data to variables and log to terminal and history.log
-// 		tracksResponseArray.map(trackInfo => {
-// 			var artist_name = trackInfo.artists[0].name;
-// 			var song_title = trackInfo.name;
-// 			var album_title = trackInfo.album.name;
-// 			var preview_link = trackInfo.preview_url ? trackInfo.preview_url : "Preview not available".err;
-
-//       console.log("\n--------------------------------------\n");
-//       console.log("Artist:", trackInfo.artists[0].name);
-// 			console.log("Song:", trackInfo.name);
-// 			console.log("Album:", trackInfo.album.name);
-// 			console.log("URL:", preview_link);
-// 			console.log("\n--------------------------------------\n");
-
-// 		});
-// 	} else {
-// 		// If 0 tracks returned log to terminal and history.log
-// 		console.log("Could not find any tracks with this title.".err);
-// 	}
-
-// });
-
-
-
-
-
-
-// var Twitter = require('twitter');
+var Twitter = require('twitter');
  
-// var client = new Twitter(keys.twitter);
+var client = new Twitter(keys.twitter);
 
-//   var params = {screen_name: 'shrinkymachine'};
-//   client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//     if (!error) {
-//       for (var i = 0; i < 3; i++) {
-//         console.log(tweets[i].text + "\nCreated on: " + tweets[i].created_at);
-//       }
-//     }
-//   });
+  var params = {screen_name: 'shrinkymachine'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      for (var i = 0; i < 20; i++) {
+        console.log(tweets[i].text + "\nCreated on: " + tweets[i].created_at);
+      }
+    }
+  });
+}
